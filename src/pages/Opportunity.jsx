5b3055765fe5488aba1e15d78c9d6bc8f0d71554
anas-ero <anassanda111@gmail.com>
2025-12-13 20:37:54 +0100

    modified:   src/pages/Opportunities.jsx

--- src/pages/Opportunity.jsx
+++ src/pages/Opportunity.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+
 const Opportunity = () => {
   const list = useSelector(state => state.opportunity.opportunities);
   console.log(list);
-  
+
   return (
     <div>
-      <h1>Opps</h1>
-    {list.map((opp) => (
-      <div key={opp.id}>
-        <h2>{opp.entreprise}</h2>
-        <p>{opp.description}</p>
-        <p>{opp.montant}</p>
+      <h1 className=''>List of opportunities</h1>
+      <div className='card p-2'>
+        {list.map((item) => (
+          <div key={item.id} className='card-item'>
+            <h2>Entreprise : {item.entreprise}</h2>
+            <p>Contact: {item.contact}</p>
+            <p>Email: {item.email}</p>
+            <p>Téléphone: {item.telephone}</p>
+            <p>Montant: {item.montant}</p>
+            <p>Probabilité: {item.probabilite}</p>
+            <p>Étape: {item.etape}</p>
+            <p>Date de Clôture: {item.dateCloture}</p>
+            <p>Source: {item.source}</p>
+          </div>))}
       </div>
-    ))}
     </div>
   )
 }
